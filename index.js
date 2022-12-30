6afb6328f758af37eb6ce56db32f36631a99f245
ritesh <riteshyadav@edulab.in>
2022-12-30 13:18:44 +0530

    ry-feat-search api mangooss

--- index.js
+++ index.js
@@ -5,31 +5,22 @@ const app = express();
 
 app.use(express.json())
 
- app.post("/create",async (req,resp)=>{
-    let data = new products (req.body);
-    const result =await data.save();
-   
-    resp.send("result");
-});
-app.get ("/list",async(req,resp)=>{
-    let data= await products.find();
-    resp.send(data);
-})
-app.delete ("/delete/:_id",async(req,resp)=>{
-    console.log(req.params)
-    let data = await products.deleteOne(req.params);
-    resp.send("data");
-})
-app.put("/update/:_id",async(req,resp)=>{
-    console.log(req,params)
-    let data=await products.updateOne(
-        rep.params,
+app.get("/search/:key",async(req,resp)=>{
+    console.log(req.params.key)
+    let data = await products.find(
         {
-            $set:req.body
+            "$or":[
+
+                {"name":{$regex:req.params.key}},
+                {"brand":{$regex:req.params.key}},
+                {"cateogary":{$regex:req.params.key}}
+            ]
         }
-    );
+    )
     resp.send(data)
 })
+ 
+
 app.listen(4000);
 
 
