af080ccba67d831cac13a379fced29508a23602f
ritesh <riteshyadav@edulab.in>
2022-12-30 12:35:43 +0530

    ry- feat-monoose-crud

--- index.js
+++ index.js
@@ -2,11 +2,34 @@ const express =require('express');
 require('./config');
 const products = require('./products');
 const app = express();
+
 app.use(express.json())
- app.post("/create",(req,resp)=>{
-    console.log(req.body);
-    resp.send("done");
+
+ app.post("/create",async (req,resp)=>{
+    let data = new products (req.body);
+    const result =await data.save();
+   
+    resp.send("result");
 });
-app.listen(5000);
+app.get ("/list",async(req,resp)=>{
+    let data= await products.find();
+    resp.send(data);
+})
+app.delete ("/delete/:_id",async(req,resp)=>{
+    console.log(req.params)
+    let data = await products.deleteOne(req.params);
+    resp.send("data");
+})
+app.put("/update/:_id",async(req,resp)=>{
+    console.log(req,params)
+    let data=await products.updateOne(
+        rep.params,
+        {
+            $set:req.body
+        }
+    );
+    resp.send(data)
+})
+app.listen(4000);
 
 
